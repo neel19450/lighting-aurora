c483cd83fa87c441a69f989db9c93c98308c3759
neel19450 <72644667+neel19450@users.noreply.github.com>
2021-09-27 22:10:13 +0530

    Create Lightning_Aurora.py

---
+++ Lightning_Aurora.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/Paramatin-op/Black-Lightning-2 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
